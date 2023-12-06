8020188bb76af065e0ccbbae7db879f960220b83
Annie Nusbaum <annie.nusbaum.83@gmail.com>
2023-12-06 09:27:33 -0500

    weatherapp

---
+++ WeatherApp/Program.cs
@@ -0,0 +1,16 @@
+﻿using Newtonsoft.Json.Linq;
+using WeatherApp;
+
+string key = File.ReadAllText("appsettings.json");
+string APIKey = JObject.Parse(key).GetValue("APIKey").ToString();
+
+
+Console.WriteLine("Please enter your city");
+var cityname = Console.ReadLine();
+
+var apiCall = $"https://api.openweathermap.org/data/2.5/weather?q={cityname}&appid={APIKey}&units=imperial\n";
+
+Console.WriteLine();
+
+Console.WriteLine($"It is currently {WeatherMap.GetTemp(apiCall)} degrees F in your location.");
+
